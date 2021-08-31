6413e96b2172610a66c0a36848b6582fd58716aa
Piotr Januszkiewicz <30535818+piotrowski128@users.noreply.github.com>
2021-08-31 23:04:46 +0200

    first commit

---
+++ pyRPG/pythonRPG.py
@@ -0,0 +1,25 @@
+
+#Lets start with smth classical: name, money and hp. based on that
+#I can start building a story
+class Player:
+    def __init__(self, Name, Money, HealthPoints):
+        self.name = Name
+        self.money = Money
+        self.hp = HealthPoints
+        
+    def spend_money(self, amount):
+        self.money -= amount
+
+    def lose_health(self, amount):
+        self.hp -= amount
+
+class Choices:
+    def __init__(self):
+        self.choices = []
+
+    def get_choice(self, pos):
+        if pos <= len(choices):
+            return self.choices[pos]
+        else: return None
+
+   
